35f83c4b98885051e9e53f6042a437b104e6e5d4
Uriel Novaczyk <novacuri2@hotmail.com>
2023-05-10 19:02:57 -0300

    up

--- prueba.js
+++ prueba.js
@@ -1,4 +1,4 @@
-const productTemplate = document.querySelector('#product-template');
+/* const productTemplate = document.querySelector('#product-template');
 const productContainer = document.querySelector('#product-list');
 
 const products = [
@@ -15,4 +15,57 @@ products.forEach(product => {
     instance.querySelector('.product-price').textContent = product.price;
     productContainer.appendChild(instance);
 });
+ */
+const productTemplate = document.querySelector("#product-template");
+// const productContainer = document.querySelector("#product-list");
 
+console.log("Variable productTemplate es:", productTemplate);
+// console.log("Variable productContainer es:", productContainer);
+
+const products = [
+	{
+		name: "Product 1",
+		description: "Description 1",
+		price: "$10",
+		imageUrl:
+			"https://www.plenainclusion.org/wp-content/uploads/2021/06/ilustracion-mujer-banco-dinero-hucha.png",
+	},
+	{
+		name: "Product 2",
+		description: "Description 2",
+		price: "$20",
+		imageUrl:
+			"https://smoda.elpais.com/wp-content/uploads/2022/03/Dinero-940x702.jpg",
+	},
+];
+
+/* products.forEach((product) => {
+	const instance = productTemplate.textContent.cloneNode(true);
+	instance.querySelector(".product-image").src = product.imageUrl;
+	instance.querySelector(".product-name").textContent = product.name;
+	instance.querySelector(".product-description").textContent =
+		product.description;
+	instance.querySelector(".product-price").textContent = product.price;
+	productContainer.appendChild(instance);
+}); */
+
+// Código prueba JPPE --------------------------------------------------------
+
+//Templates generales para armar la estructura HTML con JavaScript
+const populateProductTemplate = (product) => {
+	return `<div class="product">
+                <img class="product-image" src="${product.imageUrl}" alt="">
+                <h2 class="product-name">${product.name}</h2>
+                <p class="product-description">${product.description}</p>
+                <span class="product-price">${product.price}</span>
+            </div>`;
+};
+//Recorro el array de productos y armo los datos para cargarlos en pantalla
+const loadProducts = () => {
+	productTemplate.innerHTML = "";
+	products.forEach(
+		(product) =>
+			(productTemplate.innerHTML += populateProductTemplate(product))
+	);
+};
+loadProducts();
